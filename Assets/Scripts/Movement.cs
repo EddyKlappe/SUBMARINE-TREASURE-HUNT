212ec998ba3c9c26a6cdcfcc464833a8ecff775c
EddyKlappe <eddyklappe@Eddys-MacBook-Pro.local>
2018-06-29 17:44:39 +0200

    Git
    Git

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float force;
+	public float forceSteering;
+	public float rotateSpeed;
+	private bool buttonPressed;
+
+	public ParticleSystem bubbles;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		transform.position = new Vector3(0.0f, 1.5f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+		if (Input.GetKeyDown("space")) {
+			boostForward ();
+			bubbles.Play ();
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (new Vector3 (0, 1, 0) * rotateSpeed * Time.deltaTime);
+			rb.AddRelativeForce (new Vector3 (-1,1,-1) * forceSteering, ForceMode.VelocityChange);
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			transform.Rotate(new Vector3(0,-1,0) * rotateSpeed * Time.deltaTime);
+			rb.AddRelativeForce(new Vector3(1,1,1)*forceSteering,ForceMode.VelocityChange);
+		}
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		//rb.AddForce(new Vector3(0,1,0)*force,ForceMode.Acceleration)
+	}
+
+	void boostForward () {
+		rb.AddRelativeForce(new Vector3(3,3,0)*force,ForceMode.Acceleration);
+	}
+}
---
+++ Assets/Scripts/PickUpTreasure.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PickUpTreasure : MonoBehaviour {
+	public GameObject StoredCollider;
+	public GameObject Treasure1;
+	public GameObject Treasure2;
+	public GameObject Treasure3;
+	public GameObject TreasureHook;
+	public GameObject TreasureStored1;
+	public GameObject TreasureStored2;
+	public GameObject TreasureStored3;
+
+	public AudioClip PickUp;
+	AudioSource audioSource;
+
+	private int TreasureCount;
+
+	bool PickedThreasureUp;
+	bool Treasure1Hooked;
+	bool Treasure2Hooked;
+	bool Treasure3Hooked;
+
+	void Start () {
+		TreasureHook.SetActive(false);
+		TreasureStored1.SetActive(false);
+		TreasureStored2.SetActive(false);
+		TreasureStored3.SetActive(false);
+
+		audioSource = GetComponent<AudioSource>();
+
+		TreasureCount = 0;
+	}
+
+	void Update ()
+	{
+		if (TreasureCount == 3) 
+		{
+			SceneManager.LoadScene("EndMenuScene");
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (PickedThreasureUp == false) {
+			audioSource.PlayOneShot(PickUp);
+
+			if (other.gameObject.CompareTag("Treasure1"))
+			{
+				Treasure1.SetActive (false);
+				TreasureHook.SetActive(true);
+				PickedThreasureUp = true;
+				Treasure1Hooked = true;
+				Treasure2Hooked = false;
+				Treasure3Hooked = false;
+				return;
+			}
+
+			if (other.gameObject.CompareTag("Treasure2"))
+			{
+				Treasure2.SetActive (false);
+				TreasureHook.SetActive(true);
+				PickedThreasureUp = true;
+				Treasure1Hooked = false;
+				Treasure2Hooked = true;
+				Treasure3Hooked = false;
+				return;
+			}
+
+			if (other.gameObject.CompareTag("Treasure3"))
+			{
+				Treasure3.SetActive (false);
+				TreasureHook.SetActive(true);
+				PickedThreasureUp = true;
+				Treasure1Hooked = false;
+				Treasure2Hooked = false;
+				Treasure3Hooked = true;
+				return;
+			}
+
+			if (other.gameObject.CompareTag("Storage"))
+			{
+				return;
+			}
+		}
+		
+		if (PickedThreasureUp == true) {
+			if (other.gameObject.CompareTag("Storage"))
+			{
+				TreasureHook.SetActive(false);
+				PickedThreasureUp = false;
+
+				if (Treasure1Hooked == true) {
+					TreasureStored1.SetActive(true);
+					TreasureCount = TreasureCount + 1;
+					return;
+				}
+
+				if (Treasure2Hooked == true) {
+					TreasureStored2.SetActive(true);
+					TreasureCount = TreasureCount + 1;
+					return;
+				}
+
+				if (Treasure3Hooked == true) {
+					TreasureStored3.SetActive(true);
+					TreasureCount = TreasureCount + 1;
+					return;
+				}
+			}
+
+		}
+
+	}
+		
+			
+}
